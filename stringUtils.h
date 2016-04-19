e05389d2500cf32475d86dca08fec6361a59d6a7
Michael Peterson <mikebean233@yahoo.com>
2016-04-19 01:11:50 -0700

    no message

--- stringUtils.h
+++ stringUtils.h
@@ -1,8 +1,8 @@
-#ifndef MOUSE_PAW_GAMES_ASSIGNMENT_STRINGUTILS_H
-#define MOUSE_PAW_GAMES_ASSIGNMENT_STRINGUTILS_H
+#ifndef STRINGUTILS_H
+#define STRINGUTILS_H
 
 int stringCompareIgnoreCase(char* a, char* b);
 int stripNewLines(char* input);
 
 
-#endif //MOUSE_PAW_GAMES_ASSIGNMENT_STRINGUTILS_H
+#endif //STRINGUTILS_H
